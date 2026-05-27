ab24112092d92f75fa2903f3675f715cfc50136b
Xiaoyue Ma <xiaoym@umich.edu>
2026-05-26 22:12:05 -0700

    Remove archived prototype files

--- archive/formal/DQMC.lean
+++
@@ -1,8 +0,0 @@
-import DQMC.Basic
-import DQMC.Meas
-import DQMC.Obs
-import DQMC.RepoMap
-import DQMC.Current
-import DQMC.Density
-import DQMC.Green
-import DQMC.Measurement
--- archive/formal/lakefile.lean
+++
@@ -1,7 +0,0 @@
-import Lake
-open Lake DSL
-
-package DQMCFormal where
-
-@[default_target]
-lean_lib DQMC where
